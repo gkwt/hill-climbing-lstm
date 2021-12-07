4c8c72195e1d6fe8600e177055efc451ae609c28
Gary <gary.tom@mail.utoronto.ca>
2021-12-07 09:17:47 -0500

    First commit.

---
+++ submit-job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=rrg-aspuru
+#SBATCH --gres=gpu:1              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=6         # CPU cores/threads
+#SBATCH --mem=12000M              # memory per node
+#SBATCH --time=0-02:00            # time (DD-HH:MM)
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+module load python/3.7 scipy-stack
+module load StdEnv/2020 gcc/9.3.0
+module load rdkit/2021.03.3
+
+source ~/env/invest-vae/bin/activate
+
+time python train.py
+
+deactivate
+
